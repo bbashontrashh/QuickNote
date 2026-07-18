1121dd253f07db204f0bb9e345d69c015740d864
bbashontrashh <aitorrotger@gmail.com>
2026-07-18 13:01:05 +0200

    Write on Window

--- app/gui.py
+++ app/gui.py
@@ -9,13 +9,19 @@ class App(ctk.CTk):
         self.title("QuickNote")
         self.minsize(width=400, height=450)
         self.maxsize(width=400, height=450)
-    
+
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
+        self.textbox = ctk.CTkTextbox(master=self, width=400, height=450, corner_radius=0, font=("Consolas", 21, "bold"), text_color=("purple"))
+        self.textbox.grid(row=0, column=0, sticky="nsew")
+
 
         #WIDGETS 
+        
 
 
     #METHODS
-
         
 
 
